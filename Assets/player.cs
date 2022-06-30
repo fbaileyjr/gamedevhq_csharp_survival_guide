c1c417bebf38964520ef8f5378a5c42f03fbc95e
frankbaileyjr <frankbaileyjr@gmail.com>
2022-06-30 16:35:26 -0400

    First Challenge

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    /// <summary>
-    /// public or private reference
-    /// public = everyone access it
-    /// private = only the script can access it
-    /// data type (strings, bool, float, int)
-    /// every variable needs a name
-    /// optional -- a value
-    /// </summary>
-
-    // variable for my name
-    // variable for my age
-    // for my location
+    /// create a variable to store your name
+    /// create a variable to store your age
+    /// create a variable to store your speed
+    /// create a health variable
+    /// create a score variable
+    /// create a speed variable
+    /// variable to check if all keys were collected
+    /// variable for ammo count
 
     public string myName = "Frank";
-    public int myAge = 26;
-    public string myLocation = "Atlanta";
-    public bool hasKey = false;
+    public int myAge = 44;
+    public float mySpeed = 12.50f;
+    public int myHealth = 100;
+    public int score = 0;
+    public bool hasAllKeys = true;
+    public int ammoCount = 20;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("My name is: " + myName);
         Debug.Log("My Age is: " + myAge);
-        Debug.Log("My Location is: " + myLocation);
+        Debug.Log("My speed is: " + mySpeed);
+        Debug.Log("My health is: " + myHealth);
+        Debug.Log("My score is: " + score);
+        Debug.Log("Has all the keys been collected? " + hasAllKeys);
+        Debug.Log("My ammo count is: " + ammoCount);
+
     }
 
     // Update is called once per frame
