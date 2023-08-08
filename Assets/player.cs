baf0d1b6a679ab316240bec66273ff90f0e6e65b
unknown <frankbaileyjr@gmail.com>
2023-08-08 19:41:01 -0400

    For Loop presentation

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,48 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    /// create a variable to store your name
-    /// create a variable to store your age
-    /// create a variable to store your speed
-    /// create a health variable
-    /// create a score variable
-    /// create a speed variable
-    /// variable to check if all keys were collected
-    /// variable for ammo count
 
-    public string myName = "Frank";
-    public int myAge = 44;
-    public float mySpeed = 12.50f;
-    public int myHealth = 100;
-    public int score = 0;
-    public bool hasAllKeys = true;
-    public int ammoCount = 20;
 
+    // create a program to where we "increment" the value of how many apples we have
+    public int apples;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Debug.Log("My name is: " + myName);
-        // Debug.Log("My Age is: " + myAge);
-        // Debug.Log("My speed is: " + mySpeed);
-        // Debug.Log("My health is: " + myHealth);
-        // Debug.Log("My score is: " + score);
-        // Debug.Log("Has all the keys been collected? " + hasAllKeys);
-        // Debug.Log("My ammo count is: " + ammoCount);
-
-        for (int i = 0; i < 100; i++)
-        {
-            Debug.Log(i);
-            if ( i == 50)
-            {
-                break;
-            }
-        }
+        StartCoroutine(AddApplesRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator AddApplesRoutine()
+    {
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                apples++;
+                yield return new WaitForSeconds(.25f);
+            }
+        }
     }
 }
