9d1e5377d71137d156529bfe2501c33c21ea39ad
Frank <frankbaileyjr@gmail.com>
2023-08-09 16:47:45 -0400

    arrays

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public string[] names = new string[3];
+    // declare the size (how many elements) through the inspector
+    public string[] names;
+
+    public string[] items = new string[5];
+
+    public int[] ages = new int[] { 5, 4, 2, 55, 63 };
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log(names[0]);
+        Debug.Log(items[1]);
+        Debug.Log(ages[4]);
     }
 
     // Update is called once per frame
