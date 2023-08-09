e41c764d60f0081e42114657c07896f0c18ba027
unknown <frankbaileyjr@gmail.com>
2023-08-08 21:30:25 -0400

    Challenge: How fast you go

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,37 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Create a program where you increment the value of speed by 5 every second 
+    // when speed is greater than MaxSpeed, stop incrementing (Random between 60 and 120)
+    // Hint use a coroutine
+
+    int speed = 0;
+    int MaxSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 30; i++)
-        {
-            if (i < 11)
-            {
-                Debug.Log("i is: " + i);
-            }
-            else if(i > 10 && i < 21)
-            {
-                if (i % 2 == 0)
-                {
-                    Debug.Log("i is: " + i);
-                }
-            }
-            else if (i > 20)
-            {
-                if (i % 2 == 1)
-                {
-                    Debug.Log("i is: " + i);
-                }
-            }
-        }
+        MaxSpeed = Random.Range(60, 120);
+        Debug.Log("MaxSpeed is: " + MaxSpeed);
+        StartCoroutine(AddSpeed());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator AddSpeed()
+    {
+        while (speed < MaxSpeed)
+        {
+            Debug.Log("Current speed is: " + speed);
+            speed += 5;
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 
 }
