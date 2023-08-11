94fb2c7fddcdb26b5db6ebcfce7702d0fe721b51
unknown <frankbaileyjr@gmail.com>
2023-08-10 22:06:56 -0400

    Challenge: Pass the Object with Color

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int health;
+    // Create a program where you assign a cube in the inspector and when you hit
+    // the space key, you pass that object into a function called ChangeColor. The function should take both the GameObject
+    // and a color to change to and the operation should be handled within the function
+
+    public GameObject cube;
+    public Color colorToChangeInto;
 
     // Start is called before the first frame update
     void Start()
@@ -12,24 +17,18 @@ public class Player : MonoBehaviour
 
         
     }
-
-    public void Damage(int damageAmount)
-    {
-        health -= damageAmount;
-
-        if (health < 1)
-        {
-            health = 0;
-            Destroy(this.gameObject);
-        }
-    }
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Damage(5);
+            ChangeColor(cube, colorToChangeInto);
         }
+    }
+
+    void ChangeColor(GameObject cube, Color colorChange)
+    {
+        cube.GetComponent<MeshRenderer>().material.color = colorChange;
     }
 
 }
