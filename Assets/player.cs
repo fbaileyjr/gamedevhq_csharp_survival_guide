058ad26a52549a95cd377ad9ee49539a5cfbafa1
unknown <frankbaileyjr@gmail.com>
2023-08-10 21:09:37 -0400

    Looping Practical Example

--- Assets/player.cs
+++ Assets/player.cs
@@ -2,28 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-
-public class Item
-{
-    public int itemID;
-    public string name;
-    public string description;
-}
 public class Player : MonoBehaviour
 {
-
-    public Item[] myItems;
+    public GameObject[] cubes;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach(var item in myItems)
-        {
-            Debug.Log(item.name);
-            Debug.Log(item.itemID);
-            Debug.Log(item.description);
-        }
+        
     }
 
     // Update is called once per frame
@@ -31,11 +17,10 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            int randomID = Random.Range(0, myItems.Length);
-            Debug.Log("randomID is: " + randomID);
-            Debug.Log(myItems[randomID].description);
+            foreach (var cube in cubes)
+            {
+                cube.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
         }
     }
-
 }
- 
