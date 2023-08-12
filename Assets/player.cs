0cb572e6dd69e38a42b6c79dc9c1bf43b232b27a
Frank <frankbaileyjr@gmail.com>
2023-08-12 12:43:01 -0400

    Return Type Functions

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,31 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Create a program where you assign a cube in the inspector and when you hit
-    // the space key, you pass that object into a function called ChangeColor. The function should take both the GameObject
-    // and a color to change to and the operation should be handled within the function
-
-    public GameObject cube;
-    public Color colorToChangeInto;
+    public int total;
+    public int total2;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        total = Sum(5, 2);
+        total2 = Sum(11, 7);
         
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeColor(cube, colorToChangeInto);
-        }
     }
 
-    void ChangeColor(GameObject cube, Color colorChange)
+    private int Sum(int a, int b)
     {
-        cube.GetComponent<MeshRenderer>().material.color = colorChange;
+        Debug.Log(a + b);
+        return a + b;
     }
 
 }
