242d362d78db850e2ecb6376f85069dcda3c5007
Frank <frankbaileyjr@gmail.com>
2023-08-12 15:08:47 -0400

    Practial Presentation Return Array

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,43 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Create a program that checks if the player is dead or not. When you the space key, you should damage
-    // the player by a random amount. If the player is dead (health less than 1), print out "The player has died!"
-
-    // Bonus: Prevent the damge function from being called once player is dead and no negative health values
-
-    public int health = 100;
+    public GameObject[] players;
 
     // Start is called before the first frame update
     void Start()
     {
 
-
+        players = GetAllPlayers();
 
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsDead() == false)
-        {
-            Damage(Random.Range(5, 20));
-        }
+        
     }
 
-    public void Damage(int damageAmount)
+    GameObject[] GetAllPlayers()
     {
-        health -= damageAmount;
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
 
-        if (IsDead() == true)
+        foreach (var p in allPlayers)
         {
-            health = 0;
-            Debug.Log("The player has died!");
+            p.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
         }
-    }
 
-    public bool IsDead()
-    {
-        return health < 1;
+        return allPlayers;
     }
 
 }
