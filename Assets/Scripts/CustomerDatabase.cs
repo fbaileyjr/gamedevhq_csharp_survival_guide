1f055cf56ba63229febab0600497dca41bf98910
Frank <frankbaileyjr@gmail.com>
2023-08-23 16:23:07 -0400

    Challenge Customer Database - Part 2

--- Assets/Scripts/CustomerDatabase.cs
+++ Assets/Scripts/CustomerDatabase.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class CustomerDatabase : MonoBehaviour
 {
-    public Customer bubba;
-    public Customer spiderman;
-    public Customer johnny;
+    public Customer[] customers;
+
     // Start is called before the first frame update
     void Start()
     {
-        bubba = AddCustomer("bubba", "gump", 32, "male", "fisherman");
-        spiderman = AddCustomer("spider", "man", 23, "male", "webhead");
-        johnny = AddCustomer("johnny", "knoxville", 49, "male", "stuntman");
 
-        Debug.LogError("Name for first customer is: " + bubba.FirstName);
+        Debug.LogError("Name for first customer is: " + customers[0].FirstName);
     }
 
     // Update is called once per frame
@@ -23,9 +19,5 @@ public class CustomerDatabase : MonoBehaviour
         
     }
 
-    public Customer AddCustomer(string FirstName, string LastName, int Age, string Gender, string Occupation)
-    {
-        var customerObject = new Customer(FirstName, LastName, Age, Gender, Occupation);
-        return customerObject;
-    }
+
 }
