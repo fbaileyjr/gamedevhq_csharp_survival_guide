d002537a0f2131c4960b2a048df01caed8c08a0b
unknown <frankbaileyjr@gmail.com>
2023-10-27 22:01:51 -0400

    Bank System Inheritence Example

---
+++ Assets/Scripts/Bank.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bank
+{
+    public string branchName;
+    public string location;
+    public string cashInVault;
+
+    public void CheckBalance()
+    {
+        Debug.Log("CheckingBalance" + branchName);
+    }
+
+    public void Withdrawl()
+    {
+        Debug.Log("Withdrawling Money from:" + branchName);
+    }
+
+    public void Deposit()
+    {
+        Debug.Log("Depositinh Money to:" + branchName);
+    }
+}
