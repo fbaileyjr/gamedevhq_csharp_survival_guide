7f7b62649a342c4decc6979089c31ee629f75ed0
Frank <frankbaileyjr@gmail.com>
2024-03-16 11:58:38 -0400

    Lists

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,69 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test
-{
-    GameManager gm = new GameManager();
-    public Test()
-    {
-    }
-}
 
 
 public class GameManager : MonoBehaviour
 {
+    // list
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
 
-    public bool IsGameOver { get; private set; }
+    //array
+    public GameObject[] objectsToSpawn = new GameObject[10];
 
-    private float _speed;
-
-    public float Speed
+    private void Start()
     {
-        get
-        {
-            return _speed;
-        }
-        private set
-        {
-            _speed = value;
-        }
-    }
+        // objectsToSpawn[0] = new GameObject();
+        // enemiesToSpawn.Add(new GameObject);
+        
+        // accessing list and dict is the same
+        objectsToSpawn[2].name = "Frank";
 
-    private string _name;
-    public string Name { get; set; }
-
-    //private bool isGameOver;
-
-    //public bool IsGameOver
-    //{
-    //   get
-    //  {
-    //        return isGameOver;
-        //}
-        //set
-        //{
-        //    if (value == true)
-        //    {
-        //        Debug.Log("Oh no! The game is over!");
-        //    }
-        //    isGameOver = value;
-        //}
-    //}
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Speed = 10f;
-        Debug.Log(Speed);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            IsGameOver = true;
-        }
     }
 
 }
